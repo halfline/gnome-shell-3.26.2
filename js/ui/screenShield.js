bb05905dde41d97e48b33c8282ab70a34b2b063e
rpm-build <rpm-build>
2018-08-31 12:42:40 -0400

    0001-screenShield-unblank-when-inserting-smartcard.patch

--- js/ui/screenShield.js
+++ js/ui/screenShield.js
@@ -516,8 +516,10 @@ var ScreenShield = new Lang.Class({
         this._smartcardManager = SmartcardManager.getSmartcardManager();
         this._smartcardManager.connect('smartcard-inserted',
                                        Lang.bind(this, function(manager, token) {
-                                           if (this._isLocked && token.UsedToLogin)
+                                           if (this._isLocked && token.UsedToLogin) {
+                                               this._wakeUpScreen();
                                                this._liftShield(true, 0);
+                                           }
                                        }));
 
         this._oVirtCredentialsManager = OVirt.getOVirtCredentialsManager();
